ecba44be4ca712191a49740112b820a1bbfdbee8
Mel1o0das <m4rsik2016@yandex.ru>
2025-09-03 20:25:54 +0300

    Добавлен docker compose, начало конфигурации бд

---
+++ DirectoryService.Backend/src/DirectoryService.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using DirectoryService.Domain.Departments;
+using Microsoft.EntityFrameworkCore;
+
+namespace DirectoryService.Infrastructure;
+
+public class ApplicationDbContext : DbContext
+{
+    private readonly string _connectionString;
+
+    public ApplicationDbContext(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        
+        optionsBuilder.UseNpgsql(_connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
+
+    public DbSet<Department> Departments => Set<Department>();
+}
---
+++ DirectoryService.Backend/src/DirectoryService.Infrastructure/Configurations/DepartmentConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using DirectoryService.Domain.Departments;
+using DirectoryService.Domain.Models;
+using DirectoryService.Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DirectoryService.Infrastructure.Configurations;
+
+public class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+{
+    public void Configure(EntityTypeBuilder<Department> builder)
+    {
+        builder.ToTable("departments");
+        
+        builder.HasKey(d => d.Id).HasName("department_id");
+        
+        builder.Property(d => d.Id)
+            .HasConversion(
+                id => id.Value,
+                value => DepartmentId.Create(value));
+
+        builder.OwnsOne(d => d.Name, db =>
+        {
+            db.Property(n => n.Value)
+                .IsRequired()
+                .HasMaxLength(Constants.Text.MAX_LOW_TEXT_LENGTH)
+                .HasColumnName("department_name");
+        });
+        
+        // builder.Navigation(d => d.Name).();
+    }
+}
